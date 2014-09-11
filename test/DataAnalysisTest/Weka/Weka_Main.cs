22b54cba06675ba3a973eb1cf5894f168a83aa3b
sangwook236 <sangwook236@6302304d-92c3-67e3-8c0c-554a39276741>
2014-09-11 17:23:07 +0000

    Two projects, Core & Util are added into ${SAF_HOME}/src. Two unit testing projects, CoreUnitTest & UtilUnitTest are added into ${SAF_HOME}/unit. Two test projects, MachineLearningTest & UserInterfaceTest are added into ${SAF_HOME}/test.

--- test/DataAnalysisTest/Weka/Weka_Main.cs
+++ test/DataAnalysisTest/Weka/Weka_Main.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace SMining.DataAnalysisTest.Weka
+namespace SAF.DataAnalysisTest.Weka
 {
     class Weka_Main
     {
---
+++ unit/UtilUnitTest/UtilClassTest.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SAF.UtilUnitTest
+{
+    using SAF.Util;
+    using NUnit.Framework;
+
+    [TestFixture]
+    class UtilClassTest
+    {
+        [SetUp]
+        protected void SetUp()
+        {
+        }
+
+        [TearDown]
+        public void Cleanup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+        }
+    }
+}
